8963352660ba0736a9a105b1a3c71cb761df72a8
Aon1840 <aonattapon1840.ap@gmail.com>
2018-10-31 14:07:06 +0700

    Edit model positions

--- Positions/models.py
+++ Positions/models.py
@@ -6,12 +6,6 @@ class Position(models.Model):
     position_id = models.AutoField(primary_key=True)
     position_name = models.CharField(max_length=10, blank=False, null=False)
     is_available = models.BooleanField(blank=False, default=1)
-    x_position = models.FloatField(blank=False, null=False, default=0.0)
-    y_position = models.FloatField(blank=False, null=False, default=0.0)
-    width_scope = models.FloatField(blank=False, null=False, default=0.0)
-    height_scope = models.FloatField(blank=False, null=False, default=0.0)
-    avg_x = models.FloatField(blank=False, null=False, default=0.0)
-    avg_y = models.FloatField(blank=False, null=False, default=0.0)
     zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
     timeChange = models.DateTimeField(auto_now_add=True)
 
